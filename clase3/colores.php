5dad2d9860b55e5c0a738eeb8bb9c017f628b45e
marcos pinardi <exegeses@gmail.com>
2021-01-07 22:01:51 -0300

    muestra de colores

---
+++ clase3/colores.php
@@ -0,0 +1,38 @@
+<?php
+    $colores = [
+                'rojo', 'azul', 'verde',
+                'violeta', 'rosa', 'gris',
+                'naranja', 'celeste'
+            ];
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        body{
+            width: 850px;
+            margin: auto;
+            font-family: arial;
+        }
+        #contenedor{
+            width: 300px;
+            border: 1px solid #ccc;
+            padding: 20px;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>muestra de colores</h1>
+    <div id="contenedor">
+
+        <img src="imagenes/gris.png">
+        color gris
+
+
+    </div>
+
+</body>
+</html>

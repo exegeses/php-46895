85337045d016bffc86f4a976a57e5758992baa7d
marcos pinardi <exegeses@gmail.com>
2021-01-19 21:29:28 -0300

    panel de administración de usuarios

---
+++ sistema/adminUsuarios.php
@@ -0,0 +1,54 @@
+<?php  
+	
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Panel de administración de usuarios</h1>
+
+        <a href="admin.php" class="btn btn-outline-secondary my-2">
+            Volver a dashboard
+        </a>
+
+        <table class="table table-borderless table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                    <th colspan="2">
+                        <a href="" class="btn btn-outline-secondary">
+                            Agregar
+                        </a>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>nombre</td>
+                    <td>apell</td>
+                    <td>email</td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Modificar
+                        </a>
+                    </td>
+                    <td>
+                        <a href="" class="btn btn-outline-secondary">
+                            Eliminar
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <a href="admin.php" class="btn btn-outline-secondary my-2">
+            Volver a dashboard
+        </a>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>

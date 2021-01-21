4b633c08919002aed0d30a2aa0472028179b6a6a
marcos pinardi <exegeses@gmail.com>
2021-01-21 20:03:25 -0300

    función listarProductos()

--- sistema/adminProductos.php
+++ sistema/adminProductos.php
@@ -1,5 +1,6 @@
 <?php  
 
+
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -1,1 +1,20 @@
 <?php
+
+    ###################
+    ## CRUD de productos
+
+    function listarProductos()
+    {
+        $link = conectar();
+        $sql = "SELECT  
+                        idProducto, prdNombre, prdPrecio,
+                        p.idMarca, mkNombre, 
+                        p.idCategoria, catNombre,
+                        prdPresentacion, prdImagen
+                 FROM   productos p, marcas m, categorias c
+                 WHERE  p.idMarca = m.idMarca
+                    AND p.idCategoria = c.idCategoria";
+        $resultado = mysqli_query( $link, $sql )
+                                or die( mysqli_error($link) );
+        return $resultado;
+    }

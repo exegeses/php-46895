275ae45429ef1cac049032f2ae92ac32d032c959
marcos pinardi <exegeses@gmail.com>
2021-01-21 19:26:11 -0300

    Panel de administración de categorías

--- sistema/funciones/categorias.php
+++ sistema/funciones/categorias.php
@@ -1,1 +1,22 @@
 <?php
+
+    ###################
+    ## CRUD de categorías
+
+    function listarCategorias()
+    {
+        $link = conectar();
+        $sql = "SELECT idCategoria, catNombre
+                    FROM categorias";
+        $resultado = mysqli_query( $link, $sql )
+                            or die( mysqli_error($link) );
+        return $resultado;
+    }
+
+    /*
+     * listarCategorias()
+     * verCategoriaPorID()
+     * agregarCategoria()
+     * modificarCategoria()
+     * eliminarCategoria()
+     * */

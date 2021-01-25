42ea7982fb6170c7c6c075514a00a0a359631cf0
marcos pinardi <exegeses@gmail.com>
2021-01-25 15:58:15 -0300

    formulario de alta de una marca

---
+++ sistema/formAgregarMarca.php
@@ -0,0 +1,27 @@
+<?php  
+	
+	include 'includes/header.html';  
+	include 'includes/nav.php';  
+?>
+
+    <main class="container">
+        <h1>Alta de una marca</h1>
+
+        <div class="alert bg-light p-4 col-8 mx-auto border shadow">
+            <form action="agregarMarca.php" method="post">
+                <div class="form-group">
+                    <label for="mkNombre">Nombre de la Marca</label>
+                    <input type="text" name="MkNombre"
+                           class="form-control" id="MkNombre" required>
+                </div>
+
+                <button class="btn btn-dark my-3 mr-3 px-4">Agregar marca</button>
+                <a href="adminMarcas.php" class="btn btn-outline-secondary">
+                    Volver a panel de marcas
+                </a>
+            </form>
+        </div>
+
+    </main>
+
+<?php  include 'includes/footer.php';  ?>

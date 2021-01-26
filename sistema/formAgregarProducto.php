97479facb15d9e78b8f852cccef4ada603e679ae
marcos pinardi <exegeses@gmail.com>
2021-01-26 19:58:38 -0300

    formulario re alta de Producto, listando combos de marcas y categorias

--- sistema/formAgregarProducto.php
+++ sistema/formAgregarProducto.php
@@ -1,5 +1,10 @@
 <?php
 
+    require 'funciones/conexion.php';
+    require 'funciones/marcas.php';
+    require 'funciones/categorias.php';
+    $marcas = listarMarcas();
+    $categorias = listarCategorias();
 	include 'includes/header.html';
 	include 'includes/nav.php';  
 ?>
@@ -11,39 +16,55 @@
         <div class="alert bg-light p-4 col-8 mx-auto border shadow-sm">
             <form action="agregarProducto.php" method="post" enctype="multipart/form-data">
 
-                <div class="form-group">
+                <div class="form-group mb-4">
                     <label for="prdNombre">Nombre del Producto</label>
-                    <input type="text" name="prdNombre" class="form-control" id="prdNombre" required>
+                    <input type="text" name="prdNombre"
+                           class="form-control" id="prdNombre" required>
                 </div>
 
                 <label for="prdPrecio">Precio del Producto</label>
-                <div class="input-group mb-2">
+                <div class="input-group mb-4">
                     <div class="input-group-prepend">
                         <div class="input-group-text">$</div>
                     </div>
-                    <input type="number" name="prdPrecio" class="form-control" id="prdPrecio" min="0" step="0.01" required>
+                    <input type="number" name="prdPrecio"
+                           class="form-control" id="prdPrecio" min="0" step="0.01" required>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mb-4">
                     <label for="idMarca">Marca</label>
-                    <select class="form-control" name="idMarca" id="idMarca" required>
+                    <select class="form-select" name="idMarca" id="idMarca" required>
                         <option value="">Seleccione una marca</option>
+<?php
+            while ( $marca = mysqli_fetch_assoc($marcas) ) {
+?>
+                        <option value="<?= $marca['idMarca'] ?>"><?= $marca['mkNombre'] ?></option>
+<?php
+            }
+?>
                     </select>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mb-4">
                     <label for="idCategoria">Categoría</label>
-                    <select class="form-control" name="idCategoria" id="idCategoria" required>
+                    <select class="form-select" name="idCategoria" id="idCategoria" required>
                         <option value="">Seleccione una categoría</option>
+<?php
+            while ( $categoria = mysqli_fetch_assoc($categorias) ) {
+?>
+                        <option value="<?= $categoria['idCategoria'] ?>"><?= $categoria['catNombre'] ?></option>
+<?php
+            }
+?>
                     </select>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mb-4">
                     <label for="prdPresentacion">Presentación del Producto</label>
                     <textarea name="prdPresentacion" class="form-control" id="prdPresentacion" required></textarea>
                 </div>
 
-                <div class="form-group">
+                <div class="form-group mb-4">
                     <label for="prdStock">Stock del Producto</label>
                     <input type="number" name="prdStock" class="form-control" id="prdStock" min="0" required>
                 </div>

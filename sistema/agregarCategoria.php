e32d9a0b0a6e8bdc5ba93da461da8936f8e3aa23
marcos pinardi <exegeses@gmail.com>
2021-01-26 19:23:39 -0300

    mensaje de alta ok/error

--- sistema/agregarCategoria.php
+++ sistema/agregarCategoria.php
@@ -10,11 +10,20 @@
     <main class="container">
         <h1>Alta de una categoría</h1>
 
-        si (altaok)
-            Categoría agregada correctamente
-
-        sino
-            No se pudo agregar la categoría
+<?php
+        $css = 'danger';
+        $mensaje = 'No se pudo agregar la categoría.';
+        if( $chequeo ) {
+            $css = 'success';
+            $mensaje = 'Categoría agregada correctamente.';
+        }
+?>
+        <div class="alert alert-<?= $css ?> col-8 mx-auto">
+            <?= $mensaje ?>
+            <a href="agregarCategoria.php" class="btn btn-outline-secondary">
+                Volver a panel
+            </a>
+        </div>
 
     </main>
 

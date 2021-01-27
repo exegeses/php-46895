4641b0355cf7f15cc8c5e918f6664b7595101208
marcos pinardi <exegeses@gmail.com>
2021-01-26 21:46:00 -0300

    teoría de upload de archivos

--- clase8/procesaArchivo.php
+++ clase8/procesaArchivo.php
@@ -11,4 +11,25 @@
     echo 'Nombre temp: ', $_FILES['prdImagen']['tmp_name'];
     echo '<br>';
     echo 'Tipo: ', $_FILES['prdImagen']['type'];
+    echo '<br>';
+
+    ####################################
+    ### mover al archivo desde el /tmp
+
+    // directorio de destino
+    $ruta = 'productos/';
+    //nombre y ubicación temporal
+    $temp = $_FILES['prdImagen']['tmp_name'];
+    //renombramos archivo
+        # nombre original
+        //$prdImagen = $_FILES['prdImagen']['name'];
+        # nombre concatenado time() + extensión
+        $ext = pathinfo($_FILES['prdImagen']['name']);
+        $ext = $ext['extension'];
+        # nombre temporal sin ruta completa
+        $temporal = pathinfo($_FILES['prdImagen']['tmp_name']);
+        $temporal = $temporal['filename'];
+        $prdImagen = $temporal.'.'.$ext;
+
+    move_uploaded_file($temp, $ruta.$prdImagen);
 

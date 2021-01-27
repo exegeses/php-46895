d95578e86f06b167afbe3f7070c3a7ae20175b60
marcos pinardi <exegeses@gmail.com>
2021-01-26 21:56:18 -0300

    planificación de función subirArchivo()

--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -18,3 +18,39 @@
                                 or die( mysqli_error($link) );
         return $resultado;
     }
+
+    /**
+     * Función para subir un archivo
+     * @return string:$prdImagen
+     */
+    function subirArchivo()
+    {
+        ####
+        ##  si no enviaron archivo
+        ##         $prdImagen = 'noDisponible.jpg'
+        ##  si todo ok -> move_uploaded_file()
+        ##
+        ##
+
+        // directorio de destino
+        $ruta = 'productos/';
+        //nombre y ubicación temporal
+        $temp = $_FILES['prdImagen']['tmp_name'];
+        //renombramos archivo
+        # nombre original
+        //$prdImagen = $_FILES['prdImagen']['name'];
+        # nombre concatenado time() + extensión
+        $ext = pathinfo($_FILES['prdImagen']['name']);
+        $ext = $ext['extension'];
+        # nombre temporal sin ruta completa
+        $temporal = pathinfo($_FILES['prdImagen']['tmp_name']);
+        $temporal = $temporal['filename'];
+        $prdImagen = $temporal.'.'.$ext;
+
+        move_uploaded_file($temp, $ruta.$prdImagen);
+    }
+
+    function agregarProducto()
+    {
+        $prdImagen = subirArchivo();
+    }

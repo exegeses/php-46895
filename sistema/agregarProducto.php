96690904c27dfc7875475028a77a8165e2633704
marcos pinardi <exegeses@gmail.com>
2021-01-28 20:18:56 -0300

    aplicación de alta de un producto

--- sistema/agregarProducto.php
+++ sistema/agregarProducto.php
@@ -1,26 +1,26 @@
 <?php  
 
     require 'funciones/conexion.php';
-    require 'funciones/marcas.php';
-    $chequeo = agregarMarca();
+    require 'funciones/productos.php';
+    $chequeo = agregarProducto();
 	include 'includes/header.html';  
 	include 'includes/nav.php';  
 ?>
 
     <main class="container">
-        <h1>Alta de una marca</h1>
+        <h1>Alta de un producto</h1>
 
 <?php
         $css = 'danger';
-        $mensaje = 'No se pudo agregar la marca.';
+        $mensaje = 'No se pudo agregar el producto.';
         if( $chequeo ) {
             $css = 'success';
-            $mensaje = 'Marca agregada correctamente.';
+            $mensaje = 'Producto agregado correctamente.';
         }
 ?>
         <div class="alert alert-<?= $css ?> col-8 mx-auto">
             <?= $mensaje ?>
-            <a href="adminMarcas.php" class="btn btn-outline-secondary">
+            <a href="adminProductos.php" class="btn btn-outline-secondary">
                 Volver a panel
             </a>
         </div>

b950b1ca602188cc7fc3c6274ac3caefd1bb70a2
marcos pinardi <exegeses@gmail.com>
2021-01-28 20:03:56 -0300

    función para agregar nuevo producto

--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -25,32 +25,59 @@
      */
     function subirArchivo()
     {
-        ####
+
         ##  si no enviaron archivo
-        ##         $prdImagen = 'noDisponible.jpg'
-        ##  si todo ok -> move_uploaded_file()
-        ##
-        ##
+        $prdImagen = 'noDisponible.jpg';
 
-        // directorio de destino
-        $ruta = 'productos/';
-        //nombre y ubicación temporal
-        $temp = $_FILES['prdImagen']['tmp_name'];
-        //renombramos archivo
-        # nombre original
-        //$prdImagen = $_FILES['prdImagen']['name'];
-        # nombre concatenado time() + extensión
-        $ext = pathinfo($_FILES['prdImagen']['name']);
-        $ext = $ext['extension'];
-        # nombre temporal sin ruta completa
-        $temporal = pathinfo($_FILES['prdImagen']['tmp_name']);
-        $temporal = $temporal['filename'];
-        $prdImagen = $temporal.'.'.$ext;
+        ## si enviaron archivo y esta todo ok
+        if( $_FILES['prdImagen']['error'] == 0 ){
 
-        move_uploaded_file($temp, $ruta.$prdImagen);
+            // directorio de destino
+            $ruta = 'productos/';
+            //nombre y ubicación temporal
+            $temp = $_FILES['prdImagen']['tmp_name'];
+            //renombramos archivo
+            # nombre original
+            //$prdImagen = $_FILES['prdImagen']['name'];
+            # nombre concatenado time() + extensión
+            $ext = pathinfo($_FILES['prdImagen']['name']);
+            $ext = $ext['extension'];
+            # nombre temporal sin ruta completa
+            $temporal = pathinfo($_FILES['prdImagen']['tmp_name']);
+            $temporal = $temporal['filename'];
+            $prdImagen = $temporal.'.'.$ext;
+
+            move_uploaded_file($temp, $ruta.$prdImagen);
+        }
+        return $prdImagen;
     }
 
     function agregarProducto()
     {
+        // capturamos datos enviados por el form
+        $prdNombre = $_POST['prdNombre'];
+        $prdPrecio = $_POST['prdPrecio'];
+        $idMarca = $_POST['idMarca'];
+        $idCategoria = $_POST['idCategoria'];
+        $prdPresentacion = $_POST['prdPresentacion'];
+        $prdStock = $_POST['prdStock'];
         $prdImagen = subirArchivo();
+
+        $link = conectar();
+        $sql = "INSERT INTO productos 
+                    VALUES 
+                        (
+                            DEFAULT,
+                            '".$prdNombre."',
+                            ".$prdPrecio.",
+                            ".$idMarca.",
+                            ".$idCategoria.",
+                            '".$prdPresentacion."',
+                            '".$prdImagen."'
+                        )";
+
+        $resultado = mysqli_query($link, $sql)
+                            or die( mysqli_error($link) );
+        return $resultado;
+
     }

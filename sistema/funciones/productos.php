f5b575f4a57e99b552d795f5036f4faafd7b80a2
Marcos Pinardi <exegeses@gmail.com>
2021-02-02 20:18:17 -0300

    función para ver datos de un producto or su id

--- sistema/funciones/productos.php
+++ sistema/funciones/productos.php
@@ -82,3 +82,31 @@
         return $resultado;
 
     }
+
+    /**
+     * función para mostrar datos de un producto por su ID
+     * @return $producto:array
+     */
+    function verProductoPorID()
+    {
+        $idProducto = $_GET['idProducto'];
+        $link = conectar();
+        $sql = "SELECT  
+                        idProducto, prdNombre, prdPrecio,
+                        p.idMarca, mkNombre, 
+                        p.idCategoria, catNombre,
+                        prdPresentacion, prdStock, prdImagen
+                 FROM   productos p, marcas m, categorias c
+                 WHERE  p.idMarca = m.idMarca
+                    AND p.idCategoria = c.idCategoria
+                    AND idProducto = ".$idProducto;
+        $resultado = mysqli_query( $link, $sql )
+                        or die( mysqli_error($link) );
+        $producto = mysqli_fetch_assoc($resultado);
+        return $producto;
+    }
+
+    function modificarProducto()
+    {
+
+    }
